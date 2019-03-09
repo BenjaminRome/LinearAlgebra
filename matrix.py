9f009eb56fbf5e1a1db7270d7c15c7d7022a3f4d
Benjamin Rome <ben@benjaminrome.com>
2019-03-08 23:08:34 -0800

    started code for the add method, need to overwrite __len__

--- matrix.py
+++ matrix.py
@@ -13,8 +13,16 @@ class Matrix:
     def __str__(self):
         return str(self.matrix)
 
+    def __len__(self):
+
+
     def __add__(self, other):
-        
+        if self.height == other.length and self.width == other[0].length:
+            summed_matrix = []
+            for i in range(self.height):
+                summed_matrix.append([])
+                for j in range(self.width):
+                    summed_matrix[i][j] = self.matrix[i][j] + other[i][j]
 
     def row_swap(self, row_one, row_two):
         temp_row = []
@@ -22,5 +30,5 @@ class Matrix:
         self.matrix[row_one - 1] = (self.matrix[row_two - 1])
         self.matrix[row_two - 1] = temp_row
 
-    def row_replace(self, source_row, target_row, scalar):
+    # def row_replace(self, source_row, target_row, scalar):
 
