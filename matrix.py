c3d2ddd843bb2c2f46692a9efad27950594bcd42
Benjamin Rome <ben@benjaminrome.com>
2019-03-09 10:19:00 -0800

    Finished overwriting __len__ and finished matrix addition.

--- matrix.py
+++ matrix.py
@@ -14,18 +14,21 @@ class Matrix:
         return str(self.matrix)
 
     def __len__(self):
-
+        return int(self.height)
 
     def __add__(self, other):
-        if self.height == other.length and self.width == other[0].length:
+        if self.height == len(other) and self.width == len(other.matrix[0]):
             summed_matrix = []
             for i in range(self.height):
                 summed_matrix.append([])
                 for j in range(self.width):
-                    summed_matrix[i][j] = self.matrix[i][j] + other[i][j]
+                    summed_matrix[i].append(self.matrix[i][j] + other.matrix[i][j])
+
+            return summed_matrix
+
+        raise Exception('Invalid matrix addition; both matrices must have the same number of columns and rows')
 
     def row_swap(self, row_one, row_two):
-        temp_row = []
         temp_row = self.matrix[row_one - 1]
         self.matrix[row_one - 1] = (self.matrix[row_two - 1])
         self.matrix[row_two - 1] = temp_row
